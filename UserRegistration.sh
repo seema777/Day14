d35ed031935dbdd59412dc921a832eb7f928381b
seema777 <navaghareseema3@gmail.com>
2020-08-07 01:15:10 +1300

    Pattern For Email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -29,3 +29,18 @@ if [[ $word =~ $pat ]]
   echo Invalid
 fi
 
+
+#!/bin/bash -x
+shopt -s extglob
+echo "please enter email if it is valid or not"
+read word
+
+pat="^[a-zA-Z0-9]+([._+#-][a-zA-Z0-9]+)*@[a-zA-Z]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,2})?$"
+
+if [[ $word =~ $pat ]]
+  then
+  echo valid
+  else
+  echo invalid
+fi
+
