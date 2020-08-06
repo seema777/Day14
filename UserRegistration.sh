4af4f0730e7549f0c1cefbe50a7234f59da21f86
seema777 <navaghareseema3@gmail.com>
2020-08-07 01:32:26 +1300

    Pattern For Password

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -89,3 +89,18 @@ if [[ $password =~ $pat ]]
   echo invalid
 fi
 
+
+#!/bin/bash -x
+shopt -s extglob
+echo "please enter password"
+read password
+
+pat="^(?=.*[0-9])[a-zA-Z0-9#_]{8,}$"
+
+if [[ $password =~ $pat ]]
+  then
+  echo valid
+  else
+  echo invalid
+fi
+
