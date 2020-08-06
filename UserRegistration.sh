4264a4f950fe395b210b00d4716463ad64d2560e
seema777 <navaghareseema3@gmail.com>
2020-08-07 01:08:41 +1300

    Pattern For First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,3 +2,17 @@
 
 echo "Welcome to User Registration Problem on Master Branch"
 
+
+#!/bin/bash -x
+shopt -s extglob
+echo "please enter First Name"
+read word
+pat="^[[:upper:]][A-Z]{1}[a-zA-Z]{3,}$"
+
+if [[ $word =~ $pat ]]
+  then
+  echo Valid
+  else
+  echo Invalid
+fi
+
